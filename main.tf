c29380dfc183b70f6a2c691be8e63d4b48fe4156
Goody <jason.goodwin@cvshealth.com>
2022-09-02 15:47:36 -0400

    other end

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "google_sql_database_instance" "default" {
 
     database_flags {
       name  = "log_min_messages"
-      value = "panic"
+      value = "debug5"
     }
 
     database_flags {
