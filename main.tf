f15e2a95e1334874e18158dbdc5f11b9991cc86b
Lakshmi Naidu 9451846 <naidul1@aetna.com>
2022-10-03 21:25:09 -0500

    fix for postgresql

--- main.tf
+++ main.tf
@@ -53,45 +53,45 @@ resource "google_sql_database_instance" "default" {
 
   settings {
 
-    database_flags {
-      name  = "autovacuum"
-      value = "off"
-    }
+    # database_flags {
+    #   name  = "autovacuum"
+    #   value = "off"
+    # }
 
-    database_flags {
-      name  = "log_min_duration_statement"
-      value = -1
-    }
+    # database_flags {
+    #   name  = "log_min_duration_statement"
+    #   value = -1
+    # }
 
-    database_flags {
-      name  = "log_checkpoints"
-      value = "on"
-    }
+    # database_flags {
+    #   name  = "log_checkpoints"
+    #   value = "on"
+    # }
 
-    database_flags {
-      name  = "log_connections"
-      value = "on"
-    }
+    # database_flags {
+    #   name  = "log_connections"
+    #   value = "on"
+    # }
 
-    database_flags {
-      name  = "log_disconnections"
-      value = "on"
-    }
+    # database_flags {
+    #   name  = "log_disconnections"
+    #   value = "on"
+    # }
 
-    database_flags {
-      name  = "log_lock_waits"
-      value = "on"
-    }
+    # database_flags {
+    #   name  = "log_lock_waits"
+    #   value = "on"
+    # }
 
-    database_flags {
-      name  = "log_min_messages"
-      value = "warning"
-    }
+    # database_flags {
+    #   name  = "log_min_messages"
+    #   value = "warning"
+    # }
 
-    database_flags {
-      name  = "log_temp_files"
-      value = 0
-    }
+    # database_flags {
+    #   name  = "log_temp_files"
+    #   value = 0
+    # }
 
     database_flags {
       name  = "cloudsql.iam_authentication"
